d378ac85e9fc14498a86397b6365435ca17fbe7a
Bruno <bm.contas.oliveira@gmail.com>
2021-01-25 11:07:54 +0000

    Moved programm calls to shell. Added FN keys.

--- config.h
+++ config.h
@@ -69,91 +69,106 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-/* commands */
+/* Commands */
+// Include Fn keys
+#include <X11/XF86keysym.h>
+
+// Helper to run shell commands
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+
+// Useful functions
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", NormColorBG, "-nf", NormColorFG, "-sb", SelColorBorder, "-sf", SelColorFG, NULL };
-static const char *termcmd[]    = { "alacritty", NULL };
-static const char *browsercmd[] = { "brave", NULL };
-static const char *workcmd[]    = { "chromium", NULL };
-static const char *mediacmd[]   = { "alacritty", "-e", "ncmpcpp", NULL };
-static const char *rsscmd[]     = { "alacritty", "-e", "newsboat", NULL };
-static const char *fmgrcmd[]    = { "alacritty", "-e", "ranger", NULL };
-static const char *prtscrcmd[]  = { "flameshot", "gui", NULL };
 
 static Key keys[] = {
-	/* modifier             key             function        argument */
+	/* modifier                     key             function        argument */
         /* DWM */
-	{ MODKEY|ShiftMask,     XK_q,           quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,                   quit,           {0} },
 
-	{ MODKEY,               XK_b,           togglebar,      {0} },
-	{ MODKEY|ShiftMask,     XK_c,           killclient,     {0} },
+	{ MODKEY,                       XK_b,                   togglebar,      {0} },
+	{ MODKEY|ShiftMask,             XK_c,                   killclient,     {0} },
 
         /* Monitors */
-	{ MODKEY,               XK_KP_Insert,   focusmon,       {.i = +1 } },
-	{ MODKEY,               XK_m,           focusmon,       {.i = +1 } },
-	//{ MODKEY,             XK_comma,       focusmon,       {.i = -1 } },
-	//{ MODKEY,             XK_period,      focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,     XK_KP_Insert,   tagmon,         {.i = +1 } },
-	{ MODKEY|ShiftMask,     XK_m,           tagmon,         {.i = +1 } },
-	{ MODKEY,               XK_comma,       tagmon,         {.i = -1 } },
-	{ MODKEY,               XK_period,      tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_KP_Insert,           focusmon,       {.i = +1 } },
+	{ MODKEY,                       XK_m,                   focusmon,       {.i = +1 } },
+	//{ MODKEY,                     XK_comma,               focusmon,       {.i = -1 } },
+	//{ MODKEY,                     XK_period,              focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_KP_Insert,           tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_m,                   tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_comma,               tagmon,         {.i = -1 } },
+	{ MODKEY,                       XK_period,              tagmon,         {.i = +1 } },
 
         /* Tags */
-	{ ALTKEY,               XK_Tab,    view,                {0} },
+	{ ALTKEY,                       XK_Tab,                 view,           {0} },
 
         // Keypad without numlock
-	TAGKEYS(                XK_KP_End,      0)
-	TAGKEYS(                XK_KP_Down,     1)
-	TAGKEYS(                XK_KP_Next,     2)
-	TAGKEYS(                XK_KP_Left,     3)
-	TAGKEYS(                XK_KP_Begin,    4)
-	TAGKEYS(                XK_KP_Right,    5)
-	TAGKEYS(                XK_KP_Home,     6)
-	TAGKEYS(                XK_KP_Up,       7)
-	TAGKEYS(                XK_KP_Prior,    8)
+	TAGKEYS(                        XK_KP_End,              0)
+	TAGKEYS(                        XK_KP_Down,             1)
+	TAGKEYS(                        XK_KP_Next,             2)
+	TAGKEYS(                        XK_KP_Left,             3)
+	TAGKEYS(                        XK_KP_Begin,            4)
+	TAGKEYS(                        XK_KP_Right,            5)
+	TAGKEYS(                        XK_KP_Home,             6)
+	TAGKEYS(                        XK_KP_Up,               7)
+	TAGKEYS(                        XK_KP_Prior,            8)
 
-	{ MODKEY,               XK_0,           view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,     XK_0,           tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_0,                   view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,                   tag,            {.ui = ~0 } },
 
 	/* Layout */
         // Change layout
-	{ MODKEY|ControlMask,   XK_t,           setlayout,      {.v = &layouts[0]} },
-	{ MODKEY|ControlMask,   XK_f,           setlayout,      {.v = &layouts[1]} },
-	{ MODKEY|ControlMask,   XK_m,           setlayout,      {.v = &layouts[2]} },
-	{ MODKEY|ControlMask,   XK_space,       setlayout,      {0} },
-	{ MODKEY|ShiftMask,     XK_space,       togglefloating, {0} },
+	{ MODKEY|ControlMask,           XK_t,                   setlayout,      {.v = &layouts[0]} },
+	{ MODKEY|ControlMask,           XK_f,                   setlayout,      {.v = &layouts[1]} },
+	{ MODKEY|ControlMask,           XK_m,                   setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ControlMask,           XK_space,               setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,               togglefloating, {0} },
 
-        { MODKEY,               XK_minus,       setgaps,        {.i = -1 } },
-        { MODKEY,               XK_equal,       setgaps,        {.i = +1 } },
-        { MODKEY|ShiftMask,     XK_equal,       setgaps,        {.i = 0  } },
+        { MODKEY,                       XK_minus,               setgaps,        {.i = -1 } },
+        { MODKEY,                       XK_equal,               setgaps,        {.i = +1 } },
+        { MODKEY|ShiftMask,             XK_equal,               setgaps,        {.i = 0  } },
 
         // Change master window size
-	{ MODKEY,               XK_j,           setmfact,       {.f = -0.05} },
-	{ MODKEY,               XK_k,           setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_j,                   setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_k,                   setmfact,       {.f = +0.05} },
 
         // Move windows from slave to master and vice versa
-	{ MODKEY,               XK_i,           incnmaster,     {.i = +1 } },
-	{ MODKEY,               XK_d,           incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_i,                   incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,                   incnmaster,     {.i = -1 } },
 
         /* Windows */
         // Changed focused window
-        { MODKEY,               XK_l,           focusstack,     {.i = +1 } },
-	{ MODKEY,               XK_h,           focusstack,     {.i = -1 } },
+        { MODKEY,                       XK_l,                   focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_h,                   focusstack,     {.i = -1 } },
 
         // Move focused window to/from master
-	{ MODKEY|ShiftMask,     XK_Return,      zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Return,              zoom,           {0} },
 
         /* Programs */
-	{ MODKEY,               XK_space,       spawn,          {.v = dmenucmd } },
-	{ MODKEY,               XK_Return,      spawn,          {.v = termcmd } },
-        { MODKEY,               XK_w,           spawn,          {.v = browsercmd } },
-        { MODKEY,               XK_c,           spawn,          {.v = workcmd } },
-        { MODKEY,               XK_s,           spawn,          {.v = mediacmd } },
-        { MODKEY,               XK_r,           spawn,          {.v = rsscmd } },
-        { MODKEY,               XK_x,           spawn,          {.v = fmgrcmd } },
+	{ MODKEY,                       XK_space,               spawn,                  {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return,              spawn,                  SHCMD("$TERMINAL") },
+        { MODKEY,                       XK_w,                   spawn,                  SHCMD("$BROWSER") },
+        { MODKEY,                       XK_c,                   spawn,                  SHCMD("$WBROWSER") },
+        { MODKEY,                       XK_s,                   spawn,                  SHCMD("$TERMINAL -e $PLAYER") },
+        { MODKEY,                       XK_r,                   spawn,                  SHCMD("$TERMINAL -e $RSSREADER") },
+        { MODKEY,                       XK_x,                   spawn,                  SHCMD("$TERMINAL -e $FM") },
+
+        /* Fn Keys */
+        { 0,                            XF86XK_AudioMute,		spawn,		SHCMD("pamixer -t") },
+	{ 0,                            XF86XK_AudioRaiseVolume,	spawn,		SHCMD("pamixer --allow-boost -i 3") },
+	{ 0,                            XF86XK_AudioLowerVolume,	spawn,		SHCMD("pamixer --allow-boost -d 3") },
+        { 0,                            XF86XK_Calculator,		spawn,		SHCMD("$TERMINAL -e python") },
+
+	{ 0,                            XF86XK_AudioPlay,		spawn,		SHCMD("mpc toggle") },
+	{ 0,                            XF86XK_AudioStop,		spawn,		SHCMD("mpc stop") },
+        { 0,                            XF86XK_AudioPrev,		spawn,		SHCMD("mpc prev") },
+	{ 0,                            XF86XK_AudioNext,		spawn,		SHCMD("mpc next") },
+
+        { 0,                            XF86XK_Explorer,                spawn,          SHCMD("$TERMINAL -e $FM") },
+       	{ 0,                            XF86XK_HomePage,                spawn,		SHCMD("$BROWSER") },
 
         /* Other */
-        { 0,                    XK_Print,       spawn,          {.v = prtscrcmd } },
+        { 0,                            XK_Print,                       spawn,          SHCMD("flameshot gui") },
+        { MODKEY,			XK_Scroll_Lock,	                spawn,		SHCMD("killall screenkey || screenkey &") },
 };
 
 /* button definitions */
@@ -163,7 +178,7 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkStatusText,        0,              Button2,        spawn,          SHCMD("$TERMINAL") },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
@@ -172,4 +187,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
